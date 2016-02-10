04433965814351230663bd8cb075c6a4a7ffac5b
garderobin <garderobinshot@hotmail.com>
2016-02-10 16:44:18 -0500

    完成了几个middle,大部分middle还木有结束

--- src/GoogleOA/FlipGame.java
+++ src/GoogleOA/FlipGame.java
@@ -1,5 +1,29 @@
 package GoogleOA;
 
-public class FlipGame {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Write a function to compute all possible states of the string after one valid move.
+ * If there is no valid move, return an empty list [].
+ * @author jasmineliu
+ *
+ */
+public class FlipGame {
+	public List<String> generatePossibleNextMoves(String s) {
+		List<String> rst = new ArrayList<String>();
+		if (s == null || s.length() == 0) { return rst; }
+		for (int i = 0, len = s.length(), plusCount = 0; i < len; i++) {
+			if (s.charAt(i) == '+') {
+				if (plusCount == 0) {
+					plusCount++;
+				} else { // Find a new "++"
+					rst.add(s.substring(0, i-1) + "--" + s.substring(i+1));
+				}
+			} else {
+				plusCount = 0;
+			}
+		}
+		return rst;
+    }
 }
