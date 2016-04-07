add530672eb082bf2dd611f9a49fc3eb0b740e22
garderobin <garderobinshot@hotmail.com>
2016-04-07 18:28:50 -0400

    BigInteger finished

--- src/interview/InterviewTest.java
+++ src/interview/InterviewTest.java
@@ -14,20 +14,25 @@ public class InterviewTest {
 	public static void main(String[] args) {
 		List<String[]> test = Arrays.asList(
 				new String[]{"-5000", "-1234"},
-				new String[]{"-1234", "-5000"}
-//				new String[]{"-1234", "1234"}
+				new String[]{"-1234", "-5000"},
+				new String[]{"01234", "-5000"},
+				new String[]{"05000", "-1234"},
+				new String[]{"-5000", "+1234"},
+				new String[]{"1234", "5000"}
 				); 
 				
 		for (String[] ss: test) {
 			BigInteger b1 = new BigInteger(ss[0]), b2 = new BigInteger(ss[1]);
-			byte[] d1 = b1.getDigits(), d2 = b2.getDigits();
+//			byte[] d1 = b1.getDigits(), d2 = b2.getDigits();
 			
 //			for (int i = 0; i < d1.length; ++i) System.out.print(d1[i]);
 //			System.out.println("\n" + b1.toString());
 //			for (int i = 0; i < d2.length; ++i) System.out.print(d2[i]);
 //			System.out.println("\n" + b2.toString());
-			BigInteger b3 = b1.substract(b2);
-			System.out.println(b3.toString());
+			BigInteger b3 = b1.add(b2), b4 = b1.substract(b2);
+			System.out.println(b3.toString() + ",\t" + b4.toString());
+//			System.out.println(b1.compareAbs(b2));
+//			System.out.println(b1.compareTo(b2));
 		}
 	}
 	
--- src/interview/snapchat/BigInteger.java
+++ src/interview/snapchat/BigInteger.java
@@ -3,113 +3,58 @@ package interview.snapchat;
  * http://iwillgetthatjobatgoogle.tumblr.com/post/32583376161/writing-biginteger-better-than-jdk-one
  */
 public class BigInteger implements Comparable<BigInteger> {
-	byte[] digits; //每个byte存一个十进制的digit, 逆序从右到左存储
-	
-	public static final BigInteger ZERO = new BigInteger(new byte[]{0});
-    public static final BigInteger ONE = new BigInteger(new byte[]{1});
-	
+	boolean negative;
+	byte[] digits; //每个byte存一个十进制的digit, 正序从左到右存储, 不存符号位
 	
 	public BigInteger(byte[] digits) {
         this.digits = digits;
+        negative = false;
     }
-//	public BigInteger(byte[] digits, boolean isNegative) {
-//        this.digits = digits;
-////        this.isNegative = isNegative;
-//    }
 	
+	public BigInteger(byte[] digits, boolean negative) {
+        this.digits = digits;
+        this.negative = negative;
+    }
+	
+
 	public BigInteger(String s) {
 		s = s.trim();
-		int offset = (s.charAt(0) == '-' || s.charAt(0) == '+') ? 1 : 0, n = s.length(), sign = (s.charAt(0) == '-') ? -1 : 1;
-		this.digits = new byte[n - offset];
-		for (int i = offset, j = n - offset - 1; i < n; ++i) {
-			digits[j--] = (byte)((s.charAt(i) - '0') * sign);
+		int offset = (s.charAt(0) == '-' || s.charAt(0) == '+') ? 1 : 0, n = s.length();
+		digits = new byte[n - offset +  1];
+		negative = s.charAt(0) == '-'; 
+		for (int i = offset, j = 1; i < n; ++i) {
+			digits[j++] = (byte)((s.charAt(i) - '0'));
 		}
 	}
 	
-	public boolean isNegative() {
-		return digits != null && digits.length > 0 && digits[digits.length - 1] < 0;
-	}
 	
 	@Override
 	public String toString() {
 		if (digits == null || digits.length == 0) return "";
-		boolean isNegative = digits[digits.length - 1] < 0, trailingZero = true;
+		boolean trailingZero = true;
 		StringBuilder sb = new StringBuilder();
-		if (isNegative) sb.append('-');
-		for (int i = digits.length - 1; i >= 0; --i) {
+		
+		if (negative) sb.append('-');
+		
+		for (int i = 0; i < digits.length; ++i) {
 			trailingZero &= (digits[i] == 0);
-			if (!trailingZero) sb.append(isNegative ? (-digits[i]) : digits[i]);
+			if (!trailingZero) sb.append(digits[i]);
 		}
 		return sb.toString();
 	}
 	
-	public BigInteger add(BigInteger other) {
-        byte[] digits1, digits2;
-        if (this.digits.length >= other.digits.length) {
-            digits1 = this.digits;
-            digits2 = other.digits;
-        } else {
-            digits1 = other.digits;
-            digits2 = this.digits;
-        }
-        int carry = 0, l1 = digits1.length, l2 = digits2.length;
-        byte[] resultDigits = new byte[l1+1];
-        for (int i = 0; i < l1; ++i) {
-            int r = digits1[i] +  (i >= l2 ? 0 : digits2[i]) + carry;
-            switch (carry/10) {
-            case 0:
-            case 1:
-            case -1:
-            }
-            carry = r > 9 ? 1 : 0;
-            resultDigits[i] = (byte) (r % 10);
-        }
-        resultDigits[l1] = (byte) carry;
-        return new BigInteger(resultDigits);
-    }
 	
+	public BigInteger add(BigInteger other) { //只处理正加正
+		boolean neg1 = this.isNegative(), neg2 = other.isNegative();
+		if (neg1 ^ neg2) return neg1 ? other.substract(this, true) : this.substract(other, true);
+		else return new BigInteger(this.add(other.getDigits()), neg1);
+    }
+
 	
 	public BigInteger substract(BigInteger other) {
-//        boolean isNegative = this.compareTo(other) < 0;
-        byte[] d1 = this.digits, d2 = other.digits;
-        boolean isNeg = this.compareTo(other) < 0;
-        byte c = 0;
-        int n = d1.length, m = d2.length;
-        byte[] rst = new byte[(n>m) ? n+1 : m+1];
-        for (int i = 0, j = 0, k = 0; i < n || j < m || c == -1; ++i, ++j) {
-            byte r = (byte) (10 + d1[i] - d2[j] - c);
-            c = (byte) (r < 10 ? 1 : 0);
-            rst[k++] = (byte)( Math.abs(r%10) * (isNeg ? (-1):1)); //为操作符优先级低于加减，使用的时候必须加上括号。
-        }
-//        for (int i = 0; i < l1; ++i) {
-//            int r = digits1[i] +  (i >= l2 ? 0 : digits2[i]) + carry;
-//            carry = r > 9 ? 1 : 0;
-//            resultDigits[i] = (byte) (r % 10);
-//        }
-//        resultDigits[l1] = (byte) carry;
-        return new BigInteger(rst);
-//        byte[] digits1, digits2;
-//        if (!isNegative) {
-//            digits1 = this.digits;
-//            digits2 = other.digits;
-//        } else {
-//            digits1 = other.digits;
-//            digits2 = this.digits;
-//        }
-//        int carry = 0, l1 = digits1.length, l2 = digits2.length;
-//        byte[] resultDigits = new byte[l1+1];
-//        for (int i = 0; i < l1; i++) {
-//            int digit1 = digits1[i];
-//            int digit2 = i >= l2 ? 0 : digits2[i];
-//            int r = 10 + digit1 - digit2 - carry;
-//            carry = r > 9 ? 1 : 0;
-////            resultDigits[i] = (byte) (r % 10);
-//            resultDigits[i] = (byte)(r % 10);
-//        }
-//        resultDigits[l1] = (byte) carry;
-////        return new BigInteger(resultDigits, isNegative);
-//        return new BigInteger(resultDigits);
+		return substract(other, false);
     }
+
 	
 	@Override
     public boolean equals(Object other) {
@@ -127,28 +72,64 @@ public class BigInteger implements Comparable<BigInteger> {
     public int compareTo(BigInteger other) {
 		if (isNegative()  && !other.isNegative()) return -1;
 		if (!isNegative()  && other.isNegative()) return 1;
-		
-        byte[] digits1 = this.digits;
-        byte[] digits2 = other.getDigits();
-        int len1 = digits1.length;
-        int len2 = digits2.length;
-        for (int i = len1 - 1; i >= 0; --i) {
-            if (digits1[i] != 0) break;
-            --len1;
-        }
-        for (int i = len2 - 1; i >= 0; --i) {
-            if (digits2[i] != 0) break;
-            --len2;
-        }
-        if (len1 != len2) return isNegative() ? len2 - len1 : len1 - len2;
-        else {
-            for (int i = len1 - 1; i >= 0; --i) {
-                if (digits1[i] != digits2[i]) 
-                	return digits1[i] - digits2[i];
-            }
-        }
-        return 0;
+		else return compareAbs(other) * (isNegative() ? (-1) : 1);
     }
+	
+	
+	public int compareAbs(BigInteger other) {
+		byte[] d1 = this.digits, d2 = other.getDigits();
+        int i = 0, j = 0;
+        while (i < d1.length && d1[i++] == 0);
+        while (j < d2.length && d2[j++] == 0);
+        int len1 = d1.length - i + 1, len2 = d2.length - j + 1;
+        if (len1 != len2) return len1 - len2;
+        else {
+        	for (i -= 1, j -= 1; i < d1.length && d1[i++] == d2[j++];);
+        	return (i == d1.length) ? 0 : d1[i-1] - d2[j-1];
+        }
+	}
+	
+	byte[] add(byte[] otherDigits) {
+		int l1 = this.digits.length, l2 = otherDigits.length;
+		byte[] d1 = (l1 > l2) ? this.digits : otherDigits, d2 = (l1 > l2) ? otherDigits: this.digits;
+
+		int carry = 0;
+		byte[] rst = new byte[d1.length + 1];
+		for (int i = d1.length, j = d2.length; i > 0; --i, --j) {
+			int r = d1[i-1] +  (j < 1 ? 0 : d2[j-1]) + carry;
+			carry = r > 9 ? 1 : 0;
+			rst[i] = (byte) (r % 10);
+		}
+		rst[0] = (byte) carry;
+		return rst;
+	}
+	
+	
+	BigInteger substract(BigInteger other, boolean reverse) {
+		if (negative ^ other.isNegative() ^ reverse) {
+			return new BigInteger(this.add(other.getDigits()), negative);
+		} else {
+			int compareAbs = compareAbs(other);
+			if (compareAbs == 0) return new BigInteger(new byte[]{0});
+			boolean rstNeg = (!negative && compareAbs < 0) || (negative && compareAbs > 0);
+			byte[] rstBytes = (compareAbs < 0) ? other.substract(digits) : substract(other.digits);
+			return new BigInteger(rstBytes, rstNeg);
+		}
+    }
+	
+	
+	byte[] substract(byte[] d2) { //被减数一定是正数且一定大于减数
+		byte[] d1 = this.digits;
+		int carry = 0;
+	    byte[] rst = new byte[d1.length];
+	    for (int i = d1.length - 1, j = d2.length - 1; i > 0; --i, --j) {
+	        int r = 10 + d1[i] - (j < 0 ? 0 : d2[j]) - carry;
+	        carry = r < 10 ? 1 : 0;
+	        rst[i] = (byte) (r % 10);
+	    }
+	    rst[0] = (byte) carry;
+	    return rst;
+	}
 
 	public byte[] getDigits() {
 		return digits;
@@ -157,14 +138,13 @@ public class BigInteger implements Comparable<BigInteger> {
 	public void setDigits(byte[] digits) {
 		this.digits = digits;
 	}
-
-	public static BigInteger getZero() {
-		return ZERO;
+	
+	public boolean isNegative() {
+		return negative;
 	}
-
-	public static BigInteger getOne() {
-		return ONE;
+	
+	public void setNegative(boolean negative) {
+		this.negative = negative;
 	}
-
 	
 }
