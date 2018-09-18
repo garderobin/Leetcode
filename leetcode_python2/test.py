1f2e3b795406e0de96f2bff4b68e009df7d954fd
garderobin <garderobinshot@hotmail.com>
2018-09-18 01:45:10 -0700

    0918

--- leetcode_python2/test.py
+++ leetcode_python2/test.py
@@ -5,7 +5,22 @@ from collections import deque
 from mock0915_1_sum_of_subarray_sums import SumOfAllSubarraySumsImplSlidingWindow, SumOfAllSubarraySumsImplCounter
 
 if __name__ == "__main__":
-    nums = [0, 1, 2, 3, 4, 5]
-    for i1, e1 in enumerate(nums):
-        for i2, e2 in enumerate(nums[i1 + 1:]):
-            print i1, i2, e1, e2
+    t1 = (1, 2)
+    t2 = (3, )
+    print t2 + t1
+    # tt = (1, 2, 3, 4, 5)
+    # # print tt
+    # # print list(tt)
+    # tt2 = (6, 7, 8, 9, 10)
+    # s = set([])
+    # s.add(tt)
+    # s.add(tt2)
+    # # print s
+    #
+    # for e in s:
+    #     print e
+    # print s[-1]
+    #
+    # aa = [1, 2, 3, 4, 5]
+    # # print aa
+    # # print tuple(aa)
