d44ee16b07d56a840b17bddcf9a93a3fdd0923e7
garderobin <garderobinshot@hotmail.com>
2018-09-20 19:21:23 -0700

    hashheap datastructure

--- leetcode_python2/lc480_sliding_window_median.py
+++ leetcode_python2/lc480_sliding_window_median.py
@@ -2,6 +2,8 @@
 from abc import ABCMeta, abstractmethod
 from bisect import bisect_left, insort_left
 
+from data_structures.hashheap import HashHeap
+
 
 class SlidingWindowMedian:
     __metaclass__ = ABCMeta
@@ -96,97 +98,3 @@ class SlidingWindowMedianImplHeap(SlidingWindowMedian):
             return (self.max_heap.top()[0] + self.min_heap.top()[0]) / 2
         else:
             return self.max_heap.top()[0]
-
-
-class HashHeap:
-    """
-    使用九章算法强化班中讲到的 HashHeap。即一个 Hash + Heap。
-    Hash 的 key 是 Heap 里的每个元素，值是这个元素在 Heap 中的下标。
-
-    要做这个题首先需要先做一下 Data Stream Median。这个题是只在一个集合中增加数，不删除数，然后不断的求中点。
-    Sliding Window Median，就是不断的增加数，删除数，然后求中点。比 Data Stream Median 难的地方就在于如何支持删除数。
-
-    因为 Data Stream Median 的方法是用 两个 Heap，一个 max heap，一个min heap。所以删除的话，就需要让 heap 也支持删除操作。
-    由于 Python 的 heapq 并不支持 logn 时间内的删除操作，因此只能自己实现一个 hash + heap 的方法。
-
-    总体时间复杂度 O(nlogk)，n是元素个数，k 是 window 的大小。
-
-    TODO: 需要我自己再手打一遍：实现HashHeap这种数据结构
-    """
-
-    def __init__(self, desc=False):
-        self.hash = dict()
-        self.heap = []
-        self.desc = desc
-
-    @property
-    def size(self):
-        return len(self.heap)
-
-    def push(self, item):
-        self.heap.append(item)
-        self.hash[item] = self.size - 1
-        self._sift_up(self.size - 1)
-
-    def pop(self):
-        item = self.heap[0]
-        self.remove(item)
-        return item
-
-    def top(self):
-        return self.heap[0]
-
-    def remove(self, item):
-        if item not in self.hash:
-            return
-
-        index = self.hash[item]
-        self._swap(index, self.size - 1)
-
-        del self.hash[item]
-        self.heap.pop()
-
-        # in case of the removed item is the last item
-        if index < self.size:
-            self._sift_up(index)
-            self._sift_down(index)
-
-    # 下划线开头是private method
-    def _smaller(self, left, right):
-        return right < left if self.desc else left < right
-
-    def _sift_up(self, index):
-        while index != 0:
-            parent = index // 2
-            if self._smaller(self.heap[parent], self.heap[index]):
-                break
-            self._swap(parent, index)
-            index = parent
-
-    def _sift_down(self, index):
-        if index is None:
-            return
-        while index * 2 < self.size:
-            smallest = index
-            left = index * 2
-            right = index * 2 + 1
-
-            if self._smaller(self.heap[left], self.heap[smallest]):
-                smallest = left
-
-            if right < self.size and self._smaller(self.heap[right], self.heap[smallest]):
-                smallest = right
-
-            if smallest == index:
-                break
-
-            self._swap(index, smallest)
-            index = smallest
-
-    def _swap(self, i, j):
-        elem1 = self.heap[i]
-        elem2 = self.heap[j]
-        self.heap[i] = elem2
-        self.heap[j] = elem1
-        self.hash[elem1] = j
-        self.hash[elem2] = i
--- leetcode_python2/test.py
+++ leetcode_python2/test.py
@@ -1,26 +1,10 @@
 # coding=utf-8
+import itertools
 from _bisect import bisect_right
-from collections import deque
+from collections import deque, Counter
 
 from mock0915_1_sum_of_subarray_sums import SumOfAllSubarraySumsImplSlidingWindow, SumOfAllSubarraySumsImplCounter
 
 if __name__ == "__main__":
-    t1 = (1, 2)
-    t2 = (3, )
-    print t2 + t1
-    # tt = (1, 2, 3, 4, 5)
-    # # print tt
-    # # print list(tt)
-    # tt2 = (6, 7, 8, 9, 10)
-    # s = set([])
-    # s.add(tt)
-    # s.add(tt2)
-    # # print s
-    #
-    # for e in s:
-    #     print e
-    # print s[-1]
-    #
-    # aa = [1, 2, 3, 4, 5]
-    # # print aa
-    # # print tuple(aa)
+    seq_tuple = (1, 2, 3, 4, 5)
+    print seq_tuple[-1]
