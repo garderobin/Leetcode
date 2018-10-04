067003f179538d94847ce6ce7ce6afbd58277272
garderobin <garderobinshot@hotmail.com>
2018-10-03 22:19:45 -0700

    before 10-03

--- leetcode_python2/test.py
+++ leetcode_python2/test.py
@@ -6,8 +6,6 @@ from collections import deque, defaultdict, Counter
 from mock0915_1_sum_of_subarray_sums import SumOfAllSubarraySumsImplSlidingWindow, SumOfAllSubarraySumsImplCounter
 
 if __name__ == "__main__":
-    # deck = [1,1,2,2,2,2]
-    # counter = Counter(deck)
-    # print dict(Counter(deck))
-    print range(2, 14)
-    print list(xrange(2, 14))
+    b = [1, 3, 4, 2]
+    a = reversed(b)
+    print a
